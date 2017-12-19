9c7fb976fdc29e7c4f9a51c05a8a3258a383a5d5
otofu-square <yukita@feedforce.jp>
2017-12-19 23:51:16 +0900

    :wrench: Format handler.js

--- handler.js
+++ handler.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -8,9 +6,5 @@ module.exports.hello = (event, context, callback) => {
       input: event,
     }),
   };
-
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
