4c86fb80fadca0c523d48ca66a5614db6159b66f
otofu-square <yukita@feedforce.jp>
2017-12-20 08:06:18 +0900

    :wrench: Add webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+const slsw = require('serverless-webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const config = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.resolve(`${__dirname}/.build`),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [{ loader: 'elm-webpack-loader' }],
+      },
+    ],
+  },
+};
+
+config.plugins =
+  process.env.NODE_ENV === 'production'
+    ? config.plugins || []
+    : config.plugins.push(new UglifyJsPlugin());
+
+module.exports = config;
